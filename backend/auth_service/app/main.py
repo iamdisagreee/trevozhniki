303cb8bb2385554074859d3d08c4d958ffe54f92
vova <vavakharitonov2004@gmail.com>
2025-09-15 23:26:32 +0300

    Some changes with handling + srtucrure for auth service

---
+++ backend/auth_service/app/main.py
@@ -0,0 +1,26 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, APIRouter
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        pass
+        yield
+        pass
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+app = FastAPI(
+    title="Сервис для работы с авторизацией",
+    version="1.0.0",
+    lifespan=lifespan
+)
+
+v1_router = APIRouter(prefix="/api/v1")
+#
+# v1_router.include_router(messages.router)
+#
+# app.include_router(v1_router)
