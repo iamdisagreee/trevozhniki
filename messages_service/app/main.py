1b80a8253792774619b2c803fa3760e5b4dcccf7
vova <vavakharitonov2004@gmail.com>
2025-10-19 23:37:16 +0300

    Remove mounting

--- messages_service/app/main.py
+++ messages_service/app/main.py
@@ -34,7 +34,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount('/', StaticFiles(directory='frontend'))
+# app.mount('/', StaticFiles(directory='frontend'))
 
 v1_router = APIRouter(prefix="/api/v1")
 
